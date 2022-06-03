32a4d36db09b1ab4c3d901976ce3115d36ca4931
rebecatinnoco <rebeca.tinnoco@gmail.com>
2022-06-02 20:54:53 -0400

    jQuery first commit

---
+++ js/activities.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $("td#pid").click(function(){
+        $(this).toggleClass("red");
+    })  
+ //   $("tr").css("background-color", "green");
+    // $(".td").click(function(){
+    //     $(this).toggleClass("green");
+    // })
+})
